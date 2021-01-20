00beef3d8c8af42fdbdc1698eca24887d41ef0bf
cubamaster <muratkhid1129@gmail.com>
2021-01-20 09:57:59 -0500

    done with all points

--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SuperArray {
     private int [] arr;
 
@@ -30,6 +32,47 @@ public class SuperArray {
         }
         arr = new int[arr2.length];
         arr = arr2;
+    }
+
+    public void set(int index, int value) {
+        if (index > arr.length -1) {
+            System.out.println("ERROR: out of index");
+            return;
+        }
+        arr[index] = value;
+    }
+
+    public int getValue(int index) {
+        return arr[index];
+    }
+
+    public void growArray(int size) {
+        if (size <= arr.length) {
+            System.out.println("ERROR: size must be greater than the length of the array");
+        }
+        int [] arr2 = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[i];
+        }
+        arr = new int[size];
+        arr = arr2;
+    }
+
+    public void remove(int index) {
+        int [] arr2 = new int[arr.length-1];
+        for (int i = 0; i < index; i++) {
+            arr2[i] = arr[i];
+        }
+        for (int i = index; i < arr2.length; i++) {
+            arr2[i] = arr[i + 1];
+        }
+        arr = new int[arr.length-1];
+        arr = arr2;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(arr);
     }
 
     public int[] getArr() {
