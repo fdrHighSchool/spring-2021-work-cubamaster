0f506590893a0f8b93e1ea0bd4595d9923f80466
cubamaster <66422376+cubamaster@users.noreply.github.com>
2021-02-03 17:54:31 -0500

    Started to work on third class

---
+++ 00-SuperArray/Films.java
@@ -0,0 +1,17 @@
+public class Films {
+    public static void main(String[] args) {
+        SuperArray film_budgets = new SuperArray(0);
+        SuperArray film_box_office = new SuperArray(0);
+        film_budgets.add(237000000);
+        film_budgets.add(138000000);
+        film_budgets.add(230000000);
+        film_budgets.add(173000000);
+        film_budgets.add(153000000);
+        film_budgets.add(183000000);
+        film_budgets.add(182000000);
+        film_budgets.add(321000000);
+        film_budgets.add(127000000);
+        film_budgets.add(329000000);
+
+    }
+}
