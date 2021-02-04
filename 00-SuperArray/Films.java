3b7ccc61a12974a373ba07c4433d574b5fabef0b
cubamaster <66422376+cubamaster@users.noreply.github.com>
2021-02-04 09:02:37 -0500

    I counfused about add

--- 00-SuperArray/Films.java
+++ 00-SuperArray/Films.java
@@ -1,7 +1,7 @@
 public class Films {
-    public static void main(String[] args) {
-        SuperArray film_budgets = new SuperArray(0);
-        SuperArray film_box_office = new SuperArray(0);
+
+    SuperArray film_budgets = new SuperArray(0);
+    SuperArray film_box_office = new SuperArray(0);
         film_budgets.add(237000000);
         film_budgets.add(138000000);
         film_budgets.add(230000000);
@@ -12,6 +12,4 @@ public class Films {
         film_budgets.add(321000000);
         film_budgets.add(127000000);
         film_budgets.add(329000000);
-
-    }
 }
