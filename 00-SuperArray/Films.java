3fa2cbda8fa92aae0a4368a208e27e525bc8c35a
cubamaster <66422376+cubamaster@users.noreply.github.com>
2021-02-05 08:56:14 -0500

    added some methods

--- 00-SuperArray/Films.java
+++ 00-SuperArray/Films.java
@@ -1,20 +1,76 @@
+import java.util.Arrays;
+
+
+/**
+ * thinking to add name for every film
+ */
 public class Films {
 
-    SuperArray film_budgets = new SuperArray(0);
-    SuperArray film_box_office = new SuperArray(0);
+    SuperArray film_budgets;
+    SuperArray film_box_office;
 
     public Films()
     {
-        film_budgets.add(237000000);
-        film_budgets.add(138000000);
-        film_budgets.add(230000000);
-        film_budgets.add(173000000);
-        film_budgets.add(153000000);
-        film_budgets.add(183000000);
-        film_budgets.add(182000000);
-        film_budgets.add(321000000);
-        film_budgets.add(127000000);
-        film_budgets.add(329000000);
+        film_budgets = new SuperArray();
+        film_box_office = new SuperArray();
     }
+    public void add_budget(int value) {
+        film_budgets.add(value);
+    }
+
+    public void add_box_office(int value) {
+        film_box_office.add(value);
+    }
+
+    public int Bigestbudget() {
+        int [] arr;
+        arr = this.film_budgets.getArr();
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+
+    public int Bigest_BoxOffice() {
+        int arr[];
+        arr = this.film_box_office.getArr();
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
+    }
+
+    public int Smallesbudget() {
+        int [] arr = film_budgets.getArr();
+        Arrays.sort(arr);
+        return arr[0];
+    }
+
+    public int Smallest_BoxOffice() {
+        int [] arr = film_box_office.getArr();
+        Arrays.sort(arr);
+        return arr[0];
+    }
+
+    public int Total_budget() {
+        int sum = 0;
+        for (int i = 0; i < film_budgets.getSize(); i++) {
+            sum += film_budgets.getValue(i);
+        }
+        return sum;
+    }
+
+    public int Total_box_office() {
+        int sum = 0;
+        for (int i = 0; i < film_box_office.getSize(); i++) {
+            sum += film_box_office.getValue(i);
+        }
+        return sum;
+    }
+
+    public double average_budget() {
+        return (double) Total_budget()/film_budgets.getSize();
+    }
+
+    public double average_box_office() {
+        return (double) Total_box_office()/ film_box_office.getSize();
+    }
+
 
 }
