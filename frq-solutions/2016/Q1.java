3c6ee368e10ad038c70582afa8329c48865676fe
cubamaster <muratkhid1129@gmail.com>
2021-05-06 09:07:47 -0400

    saw some error

--- frq-solutions/2016/Q1.java
+++ frq-solutions/2016/Q1.java
@@ -4,26 +4,26 @@ import java.util.Random;
 
 public class RandomStringChooser {
     String[] words;
-    private ArrayList<String> word2 = new ArrayList<String>(Arrays.asList(words));
+    private ArrayList<String> word2 = new ArrayList<String>(Arrays.asList(words)); // 1 point
 
     public RandomStringChooser(String[] words) {
         this.words = words;
-    }
+    } // 2 point
 
     public String getNext() {
         if(word2.isEmpty())
-            return "NONE";
+            return "NONE"; // 1 point
         Random random = new Random();
         int picking_number = random.nextInt(word2.size());
         String return_word = word2.get(picking_number);
-        word2.remove(picking_number);
-        return return_word;
+        word2.remove(picking_number);// 1 point
+        return return_word; // 1 point
     }
 
     public class RandomLetterChooser extends RandomStringChooser {
         public RandomLetterChooser(String str) {
             super(getSingleLetters(str));
-        }
+        } // 2 points
 
         public static String[] getSingleLetters(String str) {
             ArrayList<String> letters = new ArrayList<>();
@@ -37,5 +37,7 @@ public class RandomStringChooser {
             return return_letters;
         }
     }
+
+    // total 8 points
 
 }
